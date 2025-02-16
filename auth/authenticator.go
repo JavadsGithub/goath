805ec124e30ac6a826173ba0bbccc3d81e5c1bc6
Javad Mohammadi <javadsemail@gmail.com>
2025-02-16 22:32:58 +0330

    add `Authenticate` method to validate tokenString

---
+++ auth/authenticator.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Authenticator interface {
+	authenticate(tokenString string) bool
+}
+
+func Authenticate(tokenString string) (bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte("It'sMySecret"), nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		fmt.Println(claims)
+		return true, nil
+	} else {
+		fmt.Println(err)
+		return false, fmt.Errorf("Token is not valid")
+	}
+}
--- main.go
+++ main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"net/http"
 
+	"github.com/JavadsGithub/goath/auth"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	r.POST("/authenticate", func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		isAuthenticated, err := auth.Authenticate(token)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err,
+			})
+		}
+
+		if isAuthenticated {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "Authorized",
+			})
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Unauthorized",
+			})
+		}
+
 	})
 
 	r.Run()
