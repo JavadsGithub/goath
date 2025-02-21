9dcae5af464cd88e6d175f06cc2cfdfbf356fe53
Javad Mohammadi <javadsemail@gmail.com>
2025-02-21 22:00:52 +0330

    update `main.go`

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/JavadsGithub/goath/auth"
+	"github.com/JavadsGithub/goath/auth/validators"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	r := gin.Default()
 
 	r.POST("/authenticate", func(c *gin.Context) {
 		token := c.GetHeader("Authorization")
-		isAuthenticated, err := auth.Authenticate(token)
+
+		validatorKey := os.Getenv("JWT_VALIDATOR")
+		if len(validatorKey) < 1 {
+			validatorKey = "default"
+		}
+		validator := validators.GetValidators()[validatorKey]
+		if validator == nil {
+			validator = validators.DefaultClaimsValidator
+		}
+
+		isAuthenticated, err := auth.Authenticate(token, validator)
 
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
@@ -22,11 +42,13 @@ func main() {
 
 		if isAuthenticated {
 			c.JSON(http.StatusOK, gin.H{
-				"msg": "Authorized",
+				"success": true,
+				"msg":     "Authorized",
 			})
 		} else {
 			c.JSON(http.StatusUnauthorized, gin.H{
-				"error": "Unauthorized",
+				"success": false,
+				"error":   "Unauthorized",
 			})
 		}
 
